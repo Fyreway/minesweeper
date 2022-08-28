3f495155e725c14b071d2283cae0dfd93fe25902
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-08-28 10:06:49 -0700

    Add buttons

---
+++ src/button.rs
@@ -0,0 +1,69 @@
+use std::path::PathBuf;
+
+use sdl2::{
+    image::LoadTexture,
+    rect::Rect,
+    render::{TextureCreator, WindowCanvas},
+};
+
+pub struct Button<'a> {
+    x: i32,
+    y: i32,
+    w: u32,
+    scale: u32,
+    img: sdl2::render::Texture<'a>,
+}
+impl Button<'_> {
+    pub fn new<'a, T: 'a>(
+        x: i32,
+        y: i32,
+        w: u32,
+        scale: u32,
+        tex_creator: &'a mut TextureCreator<T>,
+        file: PathBuf,
+    ) -> Button<'a> {
+        Button {
+            x,
+            y,
+            w,
+            scale,
+            img: tex_creator
+                .load_texture(file)
+                .expect("Could not load image"),
+        }
+    }
+
+    pub fn render(&self, canvas: &mut WindowCanvas) -> Result<(), String> {
+        canvas.copy(
+            &self.img,
+            Rect::new(0, 0, 16, 16),
+            Rect::new(self.x, self.y, 16 * self.scale, 16 * self.scale),
+        )?;
+
+        let middle_width = self.w - 32;
+
+        if middle_width > 0 {
+            canvas.copy(
+                &self.img,
+                Rect::new(16, 0, 16, 16),
+                Rect::new(
+                    self.x + 16 * self.scale as i32,
+                    self.y,
+                    middle_width * self.scale,
+                    16 * self.scale,
+                ),
+            )?;
+        }
+
+        canvas.copy(
+            &self.img,
+            Rect::new(32, 0, 16, 16),
+            Rect::new(
+                self.x + ((16 + middle_width) * self.scale) as i32,
+                self.y,
+                16 * self.scale,
+                16 * self.scale,
+            ),
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+#![allow(dead_code)]
 use rand::{rngs::ThreadRng, Rng};
-use std::fmt;
+use sdl2::{event::Event, image::InitFlag};
+use std::{fmt, path::PathBuf, time::Duration};
+
+mod button;
+use button::*;
 
 enum MapSize {
     Small,
@@ -200,23 +205,63 @@ impl fmt::Debug for Map {
     }
 }
 
-fn main() {
-    let mut map = Map::new(MapSize::Small);
+fn generate(size: MapSize) -> Map {
+    let mut map = Map::new(size);
     map.generate_mines(&mut rand::thread_rng());
     map.generate_tiles();
+    map
+}
 
-    loop {
-        match map.check_state() {
-            GameState::Win => {
-                // TODO: Display win state
-                break;
+fn main() {
+    let sdl = sdl2::init().expect("Could not start SDL");
+    let video = sdl.video().expect("Could not start video subsystem");
+    let _img = sdl2::image::init(InitFlag::PNG).expect("Could not start SDL_image");
+    let window = video
+        .window("Minesweeper", 800, 600)
+        .position_centered()
+        .build()
+        .expect("Could not create window");
+    let mut canvas = window
+        .into_canvas()
+        .accelerated()
+        .present_vsync()
+        .build()
+        .expect("Could not create canvas");
+    let mut tex_creator = canvas.texture_creator();
+    let mut events = sdl.event_pump().expect("Could not get event pump");
+    let btn = Button::new(
+        100,
+        100,
+        128,
+        5,
+        &mut tex_creator,
+        PathBuf::from("res/button.png"),
+    );
+
+    'gameloop: loop {
+        for e in events.poll_iter() {
+            match e {
+                Event::Quit { .. } => break 'gameloop,
+                _ => (),
             }
-            GameState::Lose => {
-                // TODO: Display lose state
-                break;
-            }
-            GameState::Playing => {}
         }
+        // match map.check_state() {
+        //     GameState::Win => {
+        //         // TODO: Display win state
+        //         break 'gameloop;
+        //     }
+        //     GameState::Lose => {
+        //         // TODO: Display lose state
+        //         break 'gameloop;
+        //     }
+        //     GameState::Playing => {}
+        // }
+
+        canvas.clear();
+
+        btn.render(&mut canvas).expect("Could not render button");
+
+        canvas.present();
+        std::thread::sleep(Duration::from_nanos(1_000_000_000u64 / 60));
     }
-    println!("{:?}", map);
 }
